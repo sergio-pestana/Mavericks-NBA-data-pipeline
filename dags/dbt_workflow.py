11e1bad48a21d8417b87b09809d12a7a89a97dcf
sergio-pestana <sergio.pestana@globalpay.com>
2024-01-04 18:57:46 -0300

    dbt dag

--- dags/dbt_workflow.py
+++ dags/dbt_workflow.py
@@ -4,15 +4,35 @@ from airflow.models.baseoperator import chain
 from datetime import datetime
 from datetime import timedelta
 
-from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
-
-from astro import sql as aql
-from astro.files import File
-from astro.sql.table import Table, Metadata
-from astro.constants import FileType
-
 from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
 from cosmos.airflow.task_group import DbtTaskGroup
 from cosmos.constants import LoadMode
 from cosmos.config import ProjectConfig, RenderConfig, ProfileConfig
 from pathlib import Path
+
+
+@dag(
+    start_date=datetime(2024, 1, 1),
+    schedule='@daily',
+    catchup=False,
+    dagrun_timeout=timedelta(minutes=60*4),
+    tags=['dbt workflow'],
+)
+
+def dbt_pipeline():
+
+    transform = DbtTaskGroup(
+        group_id='transform',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/daily_transform']
+        )
+    )
+
+    chain(
+        transform
+    )
+
+dbt_pipeline()
